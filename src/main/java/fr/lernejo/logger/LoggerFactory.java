43861b7d3123d7d73e2c71a65fd5f76e3d4b1bec
MA-Mousbai <mousbai@et.esiea.fr>
2021-12-03 02:29:57 +0100

    Added Composite/Contextual/File/Filtered Logger & Changed LoggerFactory

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,7 +1,10 @@
 package fr.lernejo.logger;
 
+import java.util.function.Predicate;
+
 public class LoggerFactory {
     public static Logger getLogger(String name) {
-        return new ConsoleLogger();
-    }
+        Predicate<String> fileLoggerFilter = msg -> msg.contains("simulation");
+        return new CompositeLogger(new ContextualLogger(name, new FilteredLogger(new FileLogger("logs.txt"), fileLoggerFilter)), new ContextualLogger(name, new ConsoleLogger()));    }
 }
+
